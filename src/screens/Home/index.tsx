d26cce830f598d3a02668f82e4d31bce0134a3ce
Vander Lima <vnd.vander@gmail.com>
2022-08-31 00:25:25 -0300

    feat: Form handle for add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,30 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('foi')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+      <Text style={styles.eventDate}>Sexta, 31 de Agosto de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder='Nome do particicipante'
+          placeholderTextColor={'#6b6b6b'}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.btnText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -8,13 +8,42 @@ export const styles = StyleSheet.create({
     padding: 24
   },
   eventName: {
-    color: '#fdfcfe',
+    color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48
   },
   eventDate: {
     color: '#6b6b6b',
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
+
+  input: {
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    padding: 16,
+    fontSize: 16,
+    flex: 1,
+    marginRight: 12,
+  },
+  btn: {
+    backgroundColor: '#31cf67',
+    borderRadius: 5,
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 24
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42
+  },
 })
