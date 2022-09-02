e01bb37f94d42abf8fe11dc41b8beaeb0d5426c1
Vander Lima <vnd.vander@gmail.com>
2022-09-02 13:01:55 -0300

    status bar and change scrollviewer for flatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, View, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import { Participant } from '../../components/Participant';
 
 import { styles } from './styles';
@@ -32,15 +32,25 @@ export function Home() {
           <Text style={styles.btnText}>+</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {participants.map(participant => (
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
           <Participant
-            name={participant}
-            onRemove={() => handleParticipantRemove(participant)}
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
           />
-
-        ))}
-      </ScrollView>
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text
+            style={styles.listEmptyText}
+          >
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
 
 
     </View>
